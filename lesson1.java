5340b0a0b6edea0eca5b1249c1283effa7a2deaa
Aizhan-beko <159802006+Aizhan-beko@users.noreply.github.com>
2024-02-19 19:42:28 +0600

    Create lesson1.java

---
+++ lesson1.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static double digit;
+    public static void main(String[] args) {
+
+
+    }public static void test(int num) {
+        System.out.println(num);
+        int a = 8;
+        System.out.println(a);
+        System.out.println(digit);
+
+        if(num>0){
+            System.out.println(a);
+            int b=1;
+            System.out.println(b);
+
+             digit=5;
+        }// внутри фигурных скобок if создано и нижней части не можем использовать, а "а" можем
+        // использовать(это локальные) есть глобальные
+
+
+
+        }
+    }
